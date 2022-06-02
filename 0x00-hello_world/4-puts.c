5f859ce789746645853a4502a52277d370fbb804
Nivesh <nivesh.r30@gmail.com>
2022-06-02 08:28:31 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+/*
+ * Out put text using puts
+ *
+ * returns 0 at the end
+ *
+ */
 
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
