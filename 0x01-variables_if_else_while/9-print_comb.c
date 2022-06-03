5a021245bfc06e171cf28bda4eef7553e2e9b032
Nivesh <nivesh.r30@gmail.com>
2022-06-03 06:51:19 -0700

    patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 always (success)
+ */
+int main(void)
+{
+	int i;
+
+	for  (i = 0; i < 9; i++)
+	{
+		putchar( i + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('9');
+	return (0);
+}
