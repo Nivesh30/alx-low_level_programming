9742251f5c77f7aa77b1c3640d8956d0f72b24f1
Nivesh <nivesh.r30@gmail.com>
2022-06-03 04:43:59 -0700

    alphabet ALPHABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * Return: 0 always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <='z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
