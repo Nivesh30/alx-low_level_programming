466fd601c777fc147480a9698f0e206ec636f32e
Nivesh <nivesh.r30@gmail.com>
2022-06-03 06:56:35 -0700

    patience

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,16 @@ int main(void)
 {
 	int i;
 
-	for  (i = 0; i < 9; i++)
+	for (i = 0; i < 9; i++)
 	{
 		putchar( i + '0');
+		if ( i == '9' )
+		{
+			break;
+		}
 		putchar(',');
 		putchar(' ');
 	}
-	putchar('9');
+	putchar('\n');
 	return (0);
 }
