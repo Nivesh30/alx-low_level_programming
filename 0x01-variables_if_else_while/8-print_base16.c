879666d9a8389d7f97eaba4f19da76c3b9c49c4e
Nivesh <nivesh.r30@gmail.com>
2022-06-03 06:22:07 -0700

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ *
+ *  * main - entry point
+ *
+ *   *
+ *
+ *    * Return: alway returns 0
+ *
+ *     */
+
+int main(void)
+
+{
+
+		char a;
+
+			int i;
+
+
+
+				for (i = 0; i < 10; i++)
+
+						{
+
+									putchar('0' + i);
+
+										}
+
+					
+
+					for (a = 'a'; a < 'g'; a++)
+
+							{
+
+										putchar(a);
+
+											}
+
+						putchar('\n');
+
+							return (0);
+
+}
