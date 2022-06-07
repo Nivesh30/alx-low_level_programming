9c06dd3e665631deec3e5d417b7d342fb52ad014
Nivesh <nivesh.r30@gmail.com>
2022-06-07 10:01:36 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - takes an integer
+ *
+ * @a: interger value inputted
+ *
+ * Return: a value
+ */
+int _abs(int a)
+{
+	if (a < 0)
+	{
+		return (a * -1);
+	}
+	return (a)
+}
