2bbcab3a2304ce6e54b9a8d0e3aa3006a2a94797
Nivesh <nivesh.r30@gmail.com>
2022-06-07 14:57:02 -0700

    fibo03

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,25 +6,34 @@
  */
 int main(void)
 {
-	int counter = 2;
+	long int i;
+	long int bef = 1;
+	long int aft = 2;
+	long int l = 1000000000;
+	long int bef1;
+	long int bef2;
+	long int aft1;
+	long int aft2;
 
-	float a = 1;
-	float b = a + 1;
-	float c = a + b;
-
-	printf("%.0f, ", a);
-	printf("%.0f, ", b);
-	while (counter < 98)
+	printf("%lu", bef);
+	for (i = 1; i < 91; i++)
 	{
-		counter++;
-		printf("%.0f", c);
-		a = b;
-		b = c;
-		c = a + b;
-		if (counter < 98)
-		{
-			printf(", ");
-		}
+		printf(", %lu", aft);
+		aft += bef;
+		bef = aft - bef;
+	}
+	bef1 = (bef / l);
+	bef2 = (bef % l);
+	aft1 = (aft / l);
+	aft2 = (aft % l);
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", aft1 + (aft2 / l));
+		printf("%lu", aft2 % l);
+		aft1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
+		aft2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
 	}
 	printf("\n");
 	return (0);
