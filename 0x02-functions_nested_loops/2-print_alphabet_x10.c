86e46d5f22bb1aba259c3a8fab434f0499e46cdf
Nivesh <nivesh.r30@gmail.com>
2022-06-07 06:41:22 -0700

    x10 alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: 0 always
+ */
+void print_alphabet_x10(void)
+{
+	char a;
+	int b;
+
+	for (b = 1; b <= 10; b++)
+	{
+		for(a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+}
