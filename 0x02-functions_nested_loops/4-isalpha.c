a5f1c1ef2933143be8fb654ab7cc0a0a9aeac98f
Nivesh <nivesh.r30@gmail.com>
2022-06-07 08:15:56 -0700

    isAlpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - checks input character
+ *
+ * @c: character input
+ *
+ * Retrun: 0 always
+ */
+int _isalpha(int c)
+{
+	if ((c <= 122 && c >= 97) || (c <= 90 && c >= 65))
+	{
+		return (0);
+	}
+	else
+	{
+		return (0);
+	}
+}
