3fb0178d614c28e67a323a29413d87af6fef35b1
Nivesh <nivesh.r30@gmail.com>
2022-06-07 11:47:52 -0700

    time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,7 +13,7 @@ void jack_bauer(void)
 		for (min = 0; min < 60; min++)
 		{
 			_putchar(hrs / 10 + '0');
-			_putchars(hrs % 10 + '0');
+			_putchar(hrs % 10 + '0');
 			_putchar(':');
 			_putchar(min / 10 + '0');
 			_putchar(min % 10 + '0');
