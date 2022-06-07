0d909627faf5a8fc3c6747b897d887b45c57e6c7
Nivesh <nivesh.r30@gmail.com>
2022-06-07 13:01:52 -0700

    timestable

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - prints time table
+ */
+void times_table(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			int c = a * b;
+
+			if (b == 0)
+			{
+				_putchar(c + '0');
+			}
+			else if (c >= )
+			{
+				_putchar(' ');
+				_putchar(c / 10 + '0');
+				_putchar(c % 10 + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(c + '0');
+			}
+			if (x != 9)
+			{
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
+}
