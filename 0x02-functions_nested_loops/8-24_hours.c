713101a652b967d2659eb0e7955c40f09fe9b37b
Nivesh <nivesh.r30@gmail.com>
2022-06-07 11:45:47 -0700

    time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer - returns values
+ *
+ */
+void jack_bauer(void)
+{
+	int min;
+	int hrs;
+
+	for (hrs = 0; hrs < 24; hrs++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar(hrs / 10 + '0');
+			_putchars(hrs % 10 + '0');
+			_putchar(':');
+			_putchar(min / 10 + '0');
+			_putchar(min % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
