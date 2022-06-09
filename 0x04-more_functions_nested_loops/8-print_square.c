0b3bb6998b60e800afabe0c21060e6c161ddbfec
Nivesh <nivesh.r30@gmail.com>
2022-06-09 08:39:49 -0700

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - prints square with hash symbol
+ * @size: size of the square
+ * Return: square
+ */
+void print_square(int size)
+{
+	int i;
+	int b;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
