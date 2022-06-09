28fbafe74c42e8dd8e3198b255962d8051615010
Nivesh <nivesh.r30@gmail.com>
2022-06-09 11:16:47 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,26 +9,31 @@
 
 int main(void)
 {
-	int i;
+	int i = 1;
 
-	for (i = 1; i <= 100; i++)
+	while (i <= 100)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
+		if (i % 3 ==0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("%s", "FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("%s", "Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("%s", "Buzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
+		if (i != 100)
+		{
+			printf(" ");
+		}
+		i++;
 	}
 	printf("\n");
 	return (0);
