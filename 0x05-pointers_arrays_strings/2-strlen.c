4d073176d020178a17bbd68434e518f5e0c01720
Nivesh <nivesh.r30@gmail.com>
2022-06-13 08:48:35 -0700

    str len

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - takes input to return length
+ * @s: character c
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int l;
+
+	while (*s != 0)
+	{
+		l++;
+		s++;
+	}
+	return (l);
+}
