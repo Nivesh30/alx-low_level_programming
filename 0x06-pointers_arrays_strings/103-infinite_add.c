ea70328deb873a8c05f2ef26ed89a7a598ea1297
Nivesh <nivesh.r30@gmail.com>
2022-06-16 03:00:26 -0700

    infinite

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,82 +1,155 @@
 #include "main.h"
-/**
- * infinite_add - function
- * @n1:first number
- * @n2:second number
- * @r: result
- * @size_r: buffer size
- * Return: pointer
- */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{
-	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
-	while (*(n1 + c1) != '\0')
+
+
+/**
+ *
+ *  * rev_string - reverse array
+ *
+ *   * @n: integer params
+ *
+ *    * Return: 0
+ *
+ *     */
+
+
+
+void rev_string(char *n)
+
+{
+
+	int e = 0;
+
+	int f = 0;
+
+	char temp;
+
+
+
+	while (*(n + e) != '\0')
+
 	{
-		c1++;
+
+		e++;
+
 	}
-	while (*(n2 + c2) != '\0')
+
+	e--;
+
+
+
+	for (f = 0; f < e; f++, e--)
+
 	{
-		c2++;
+
+		temp = *(n + f);
+
+		*(n + f) = *(n + e);
+
+		*(n + e) = temp;
+
 	}
-	if (c1 >= c2)
-	{
-		bg = c1;
-	}
-	else
-	{
-		bg = c2;
-	}
-	if (size_r <= bg + 1)
-	{
+
+}
+
+
+
+/**
+ *
+ *  * infinite_add - add 2 numbers together
+ *
+ *   * @n1: text representation of 1st number to add
+ *
+ *    * @n2: text representation of 2nd number to add
+ *
+ *     * @r: pointer to buffer
+ *
+ *      * @size_r: buffer size
+ *
+ *       * Return: pointer to calling function
+ *
+ *        */
+
+
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+
+{
+
+	int overflow = 0, e = 0, f = 0, digits = 0;
+
+	int val1 = 0, val2 = 0, temp_tot = 0;
+
+
+
+	while (*(n1 + e) != '\0')
+
+		e++;
+
+	while (*(n2 + f) != '\0')
+
+		f++;
+
+	e--;
+
+	f--;
+
+	if (f >= size_r || e >= size_r)
+
 		return (0);
-	}
-	r[bg + 1] = '\0';
-	c1--, c2--, size_r--;
-	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
-	while (bg >= 0)
+
+	while (f >= 0 || e >= 0 || overflow == 1)
+
 	{
-		op = dr1 + dr2 + add;
-		if (op >= 10)
-		{
-			add = op / 10;
-		}
+
+		if (e < 0)
+
+			val1 = 0;
+
 		else
-		{
-			add = 0;
-		}
-		if (op > 0)
-		{
-			*(r + bg) = (op % 10) + 48;
-		}
+
+			val1 = *(n1 + e) -'0';
+
+		if (f < 0)
+
+			val2 = 0;
+
 		else
-		{
-			*(r + bg) = '0';
-		}
-		if (c1 > 0)
-		{
-			c1--, dr1 = *(n1 + c1) - 48;
-		}
+
+			val2 = *(n2 + f) -'0';
+
+		temp_tot = val1 + val2 + overflow;
+
+		if (temp_tot >= 10)
+
+			overflow = 1;
+
 		else
-		{
-			dr1 = 0;
-		}
-		if (c2 > 0)
-		{
-			c2--, dr2 = *(n2 + c2) - 48;
-		}
-		else
-		{
-			dr2 = 0;
-		}
-		bg--, size_r--;
+
+			overflow = 0;
+
+		if (digits >= (size_r - 1))
+
+			return (0);
+
+		*(r + digits) = (temp_tot % 10) + '0';
+
+		digits++;
+
+		f--;
+
+		e--;
+
 	}
-	if (*(r) == '0')
-	{
-		return (r + 1);
-	}
-	else
-	{
-		return (r);
-	}
+
+	if (digits == size_r)
+
+		return (0);
+
+	*(r + digits) = '\0';
+
+	rev_string(r);
+
+	return (r);
+
 }
