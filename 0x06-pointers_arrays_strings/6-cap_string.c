1258ba141774c4d90698ad2877959ede754a4732
Nivesh <nivesh.r30@gmail.com>
2022-06-16 01:59:48 -0700

    cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,30 +6,25 @@
  */
 char *cap_string(char *str)
 {
-	int index = 0;
+	int e = 0;
+	int f = 0;
+	char c[] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125, 32, 10, 9};
 
-	while (str[++index])
+	while (str[e] != '\0')
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
+		if (e == 0 && str[e] >= 97 && str[e] <= 122)
 		{
-			index++;
+			str[e] = str[e] - 32;
 		}
-		if (str[index - 1] == ' ' ||
-				str[index - 1] == '\t' ||
-				str[index - 1] == '\n' ||
-				str[index - 1] == ',' ||
-				str[index - 1] == ';' ||
-				str[index - 1] == '.' ||
-				str[index - 1] == '!' ||
-				str[index - 1] == '?' ||
-				str[index - 1] == '"' ||
-				str[index - 1] == '(' ||
-				str[index - 1] == ')' ||
-				str[index - 1] == '{' ||
-				str[index - 1] == '}')
+		while (c[f] != '\0')
 		{
-			str[index] -= 32;
+			if (c[f] == str[e] && (str[e + 1] >= 97 && str[e + 1] <= 122))
+			{
+				str[e + 1] = str[e + 1] - 32;
+			}
+			f++;
 		}
+		e++;
 	}
 	return (str);
 }
