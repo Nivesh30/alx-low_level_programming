b8f007ee7865d538b08e0903cc6c2679e4f50923
Nivesh30 <105005447+Nivesh30@users.noreply.github.com>
2022-09-01 22:12:25 +0200

    Update 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,51 +1,44 @@
 #include "lists.h"
+
 /**
- * delete_dnodeint_at_index - deletes the node at index of a
- * dlistint_t linked list
- *
- * @head: head of the list
- * @index: index of the new node
- * Return: 1 if it succeeded, -1 if it failed
+ * delete_dnodeint_at_index - deletes node at given idx
+ * @head: pointer to head of doubly linked list
+ * @index: index
+ * Return: 1 if successful, -1 if failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *h1;
-	dlistint_t *h2;
-	unsigned int i;
+	dlistint_t *del = NULL;
 
-	h1 = *head;
+	/* do nothing if nothing to delete */
+	if (head == NULL || *head == NULL)
+		return (-1);
 
-	if (h1 != NULL)
-		while (h1->prev != NULL)
-			h1 = h1->prev;
+	del = *head;
 
-	i = 0;
-
-	while (h1 != NULL)
+	/* delete first node */
+	if (index == 0)
 	{
-		if (i == index)
-		{
-			if (i == 0)
-			{
-				*head = h1->next;
-				if (*head != NULL)
-					(*head)->prev = NULL;
-			}
-			else
-			{
-				h2->next = h1->next;
-
-				if (h1->next != NULL)
-					h1->next->prev = h2;
-			}
-
-			free(h1);
-			return (1);
-		}
-		h2 = h1;
-		h1 = h1->next;
-		i++;
+		*head = (*head)->next;
+		free(del);
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		return (1);
 	}
 
+	/* delete nth node as long as within range of list */
+	while ((index != 0) && (del->next != NULL))
+	{
+		index -= 1;
+		del = del->next;
+	}
+	if (index == 0)
+	{
+		del->prev->next = del->next;
+		if (del->next != NULL)
+			del->next->prev = del->prev;
+		free(del);
+		return (1);
+	}
 	return (-1);
 }
